3e50d9bd9c6322a73bec5a51022929df0b9e8d41
Naina Trisa Dalmeida <naina.dalmeida@learner.manipal.edu>
2023-07-31 19:41:12 +0530

    logo changes

--- clothing/src/assets/index.js
+++ clothing/src/assets/index.js
@@ -1,9 +1,9 @@
-import ai from "./ai.png";
-import fileIcon from "./file.png";
-import swatch from "./swatch.png";
-import download from "./download.png";
+import ai from "./icon_ai.png";
+import fileIcon from "./icons_upload_file.png";
+import swatch from "./icon_color_pallet.png";
+import download from "./download_img.png";
 
-import logoShirt from "./logo-tshirt.png";
-import stylishShirt from "./stylish-tshirt.png";
+import logoShirt from "./logo_tshirt.png";
+import stylishShirt from "./fullLogo_tshirt.png";
 
 export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
